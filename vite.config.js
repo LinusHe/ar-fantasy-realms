6fb4ba68f223ffa285f90969faba5463ae4eb0a0
Linus Herterich <linus.herterich@gmail.com>
2025-02-01 17:56:12 +0100

    init node vite setup

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import fs from 'fs'
+import path from 'path'
+
+// Note: Ensure these files exist (or generate self-signed certificates) in the "certs" folder
+const certDir = path.resolve(__dirname, 'certs')
+const https = {
+  key: fs.readFileSync(path.join(certDir, 'localhost-key.pem')),
+  cert: fs.readFileSync(path.join(certDir, 'localhost-cert.pem')),
+}
+
+export default defineConfig({
+  server: {
+    host: true,
+    https
+  }
+}) 
